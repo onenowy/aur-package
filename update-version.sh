468f1d7651e912f17dbe2f3c5b5c7d91613d1207
onenowy <92918392+onenowy@users.noreply.github.com>
2025-11-29 14:40:57 +0900

    Refactor update-version.sh for version checks
    Update version checking script for Microsoft Edge package.

--- update-version.sh
+++ update-version.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 set -e
 
-# 패키지 설정
 PKG_NAME="microsoft-edge-stable"
 REPO_BASE="https://packages.microsoft.com/yumrepos/edge"
 
 echo "Checking for updates..."
 
-# 1. 메타데이터 파일 위치 찾기 (repomd.xml 파싱)
 REPO_METADATA=$(curl -sSf "$REPO_BASE/repodata/repomd.xml" | \
     xmllint --xpath 'string(//*[local-name()="data"][@type="other"]/*[local-name()="location"]/@href)' -)
 
-# 2. 최신 버전 정보 파싱 (other.xml.gz 파싱)
 LATEST_VER=$(curl -sSf "$REPO_BASE/$REPO_METADATA" | \
     gzip -dc | \
     xmllint --xpath "string(//*[local-name()=\"package\"][@name=\"$PKG_NAME\"][last()]/*[local-name()=\"version\"]/@ver)" -)
@@ -21,7 +18,6 @@ if [ -z "$LATEST_VER" ]; then
     exit 1
 fi
 
-# 3. 로컬 PKGBUILD 버전 확인
 CURRENT_VER=$(grep "^pkgver=" PKGBUILD | cut -d'=' -f2)
 
 echo "Remote: $LATEST_VER"
@@ -33,19 +29,15 @@ if [ "$LATEST_VER" == "$CURRENT_VER" ]; then
 else
     echo "New version found! Updating PKGBUILD..."
 
-    # 버전 업데이트
     sed -i "s/^pkgver=.*/pkgver=${LATEST_VER}/" PKGBUILD
-    # 릴리스 번호 초기화
     sed -i "s/^pkgrel=.*/pkgrel=1/" PKGBUILD
 
-    # SHA256 체크섬 업데이트
     NEW_SHA256=$(curl -sSf "$REPO_BASE/$REPO_METADATA" | \
         gzip -dc | \
         xmllint --xpath "string(//*[local-name()=\"package\"][@name=\"$PKG_NAME\"][last()]/@pkgid)" -)
 
     sed -i "s/^sha256sums=('.*/sha256sums=('${NEW_SHA256}'/" PKGBUILD
 
-    # GitHub Actions 변수 출력
     echo "UPDATE_NEEDED=true" >> "$GITHUB_OUTPUT"
     echo "NEW_VERSION=${LATEST_VER}" >> "$GITHUB_OUTPUT"
 fi
