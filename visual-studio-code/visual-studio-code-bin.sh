5ab272f402ac89e96f8c586a9909b7215967e066
onenowy <onenowy@gmail.com>
2026-05-04 08:37:42 +0900

    Add VSCODE_PATH

--- visual-studio-code/visual-studio-code-bin.sh
+++ visual-studio-code/visual-studio-code-bin.sh
@@ -10,4 +10,8 @@ fi
 # Set the environment to run the CLI entry point as Node
 # This handles argument parsing correctly and avoids UUID-file errors
 export ELECTRON_RUN_AS_NODE=1
+
+# Tell VS Code where its main binary is located so it can find it for elevated operations (pkexec)
+export VSCODE_PATH="/usr/bin/code"
+
 exec /usr/bin/electron39 /opt/visual-studio-code/resources/app/out/cli.js /opt/visual-studio-code/code-launcher.js "$@" $CODE_USER_FLAGS
