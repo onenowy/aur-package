6d668ebfbcd9ced1f576995161005c3a6921fe96
onenowy <onenowy@gmail.com>
2026-07-05 01:25:04 +0900

    enable vscode

--- visual-studio-code/visual-studio-code-bin.sh
+++ visual-studio-code/visual-studio-code-bin.sh
@@ -9,8 +9,4 @@ if [[ -f $XDG_CONFIG_HOME/code-flags.conf ]]; then
 fi
 
 # 2. Launch sequence
-# We launch the loader script directly. This ensures Electron starts in GUI mode
-# and correctly initializes the environment before handing control to VS Code.
-exec /usr/bin/electron39 \
-    /opt/visual-studio-code/code-loader.mjs \
-    "$@" $CODE_USER_FLAGS
+exec /opt/visual-studio-code/bin/code "$@" $CODE_USER_FLAGS
